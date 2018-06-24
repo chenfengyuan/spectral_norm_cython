9921baad683ef06dc8f50ce3742f09e4dfcd816b
Fengyuan Chen <cfy1990@gmail.com>
2018-06-24 15:58:56 +0800

    minor changes

--- impl.pyx
+++ impl.pyx
@@ -1,10 +1,13 @@
 # distutils: language = c++
 # cython: cdivision=True
 
+
 from libcpp.vector cimport vector
 from libc.math cimport sqrt
 from cython.parallel import prange
+
 cdef int N_CPU = 4
+
 cdef int A(int i, int j) nogil:
     return ((i+j) * (i+j+1) / 2 + i + 1)
 
@@ -46,23 +49,25 @@ cdef void mult_Atv(vector[double] & v, vector[double] & out, int n):
     for i in prange(n, nogil=True, num_threads=N_CPU):
         mult_Atv_helper(v, out, i, n)
 
-cdef void mult_AtAv(vector[double] & v, vector[double] & out, int n):
-    cdef vector[double] tmp=vector[double](n)
+cdef void mult_AtAv(vector[double] & tmp, vector[double] & v, vector[double] & out, int n):
     mult_Av(v, tmp, n)
     mult_Atv(tmp, out, n)
 
 
 cpdef main():
+    cdef int n
     import sys
     n = int(sys.argv[1])
     cdef vector[double] u = vector[double]()
     cdef vector[double] v = vector[double]()
+    cdef vector[double] tmp=vector[double]()
     cdef int i
     u.resize(n, 1)
     v.resize(n)
+    tmp.resize(n)
     for i in range(10):
-        mult_AtAv(u, v, n)
-        mult_AtAv(v, u, n)
+        mult_AtAv(tmp, u, v, n)
+        mult_AtAv(tmp, v, u, n)
     print('%.9f' % sqrt(dot(u,v,n) / dot(v,v,n)))
 
 
